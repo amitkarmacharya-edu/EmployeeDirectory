fb6d52e57a49ef91b29d03995b4c062933a068ce
amitkarmacharya-edu <amit.education2018@gmail.com>
2021-02-09 11:03:24 -0500

    connected app and package json

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React from "react";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
 import Main from "./component/Main";
-import Row from "./component/Row";
 import Employees from "./component/Employees";
 import "bootstrap/dist/css/bootstrap.css";
-import './App.css';
 
 function App() {
   return (
