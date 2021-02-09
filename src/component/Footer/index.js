c5c8a6c5b1c81e9d2396985705b21154a7f09329
amitkarmacharya-edu <amit.education2018@gmail.com>
2021-02-09 09:09:31 -0500

    created react app and file structure for the project

---
+++ src/component/Footer/index.js
@@ -0,0 +1,9 @@
+    
+import React from "react";
+import "./style.css";
+
+function Footer(props) {
+  return <footer className="container-fluid text-center py-2 bg-dark text-white footer">&copy;copyright 2021</footer>;
+}
+
+export default Footer
---
+++ src/component/Main/index.js
@@ -0,0 +1,9 @@
+    
+import React from "react";
+import "./style.css";
+
+function Main(props) {
+  return <main className="main">{props.children}</main>;
+}
+
+export default Main
