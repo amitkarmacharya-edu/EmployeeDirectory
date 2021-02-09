6e6d0fe50a468e6b4648eddd9c6b20e249374038
amitkarmacharya-edu <amit.education2018@gmail.com>
2021-02-09 09:09:50 -0500

    created footer component

--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -6,4 +6,4 @@ function Footer(props) {
   return <footer className="container-fluid text-center py-2 bg-dark text-white footer">&copy;copyright 2021</footer>;
 }
 
-export default Footer
+export default Footer;
